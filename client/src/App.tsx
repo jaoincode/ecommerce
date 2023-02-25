02714aa05ca56c098f0163fcc9fce794bdf585a1
jaoincode <ojoaogugel@gmail.com>
2023-02-24 22:59:49 -0300

    creating router layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,42 @@
 import "./App.css";
 
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Products from "./pages/Products";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function Layout() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <>Home</>,
-  },
-  {
-    path: "/products/:id",
-    element: <>Category</>,
-  },
-  {
-    path: "/product/:id",
-    element: <>Product</>,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id?",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ],
   },
 ]);
 
